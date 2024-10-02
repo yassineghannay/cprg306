2ba7629cd27ef96d695aa006d367e683ea8bb4a5
Yassine Ghannay <yassineghannay13@gmail.com>
2024-10-01 23:15:27 -0600

    week3 commit

--- cprg306-assignments/app/page.js
+++ cprg306-assignments/app/page.js
@@ -8,8 +8,12 @@ export default function Page() {
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <p>
-         <Link href="/week-2">Week 2 Assignment</Link>
+        <Link href="/week-2">Week 2 Assignment</Link>
+      </p>
+      <p>
+        <Link href="/week-3">Week 3 Assignment</Link>
       </p>
     </main>
   );
 }
+
---
+++ cprg306-assignments/app/week-3/page.js
@@ -0,0 +1,13 @@
+// /app/week-3/page.js
+
+import React from 'react';
+import ItemList from './item-list'; // Default import
+
+export default function Page() {
+  return (
+    <main className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Shopping List</h1>
+      <ItemList />
+    </main>
+  );
+}
